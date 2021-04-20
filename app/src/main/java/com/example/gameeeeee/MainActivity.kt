ed44fe123e577e4678bcb1d0b4afc60085a606be
DeepDweller <misharozental@gmail.com>
2021-04-20 17:01:04 +0700

    Sound Effect

--- app/src/main/java/com/example/gameeeeee/MainActivity.kt
+++ app/src/main/java/com/example/gameeeeee/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.gameeeeee
 
+import android.media.AudioManager
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -12,18 +14,28 @@ import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
 
+    private var soundPool: SoundPool? = null
+    private var soundId1: Int = 0
+    private var soundId2: Int = 0
+    private var soundId3: Int = 0
+    private var soundId4: Int = 0
+    private var soundId5: Int = 0
+    private var soundId6: Int = 0
+    var play_sound_buffer = 0
+
+
 /*lateinit var Courutine_Dice : CoroutineScope*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        //Найти элемент верстки (Картинка) по id. поставить на него слушатель кликов
         findViewById<ImageView>(R.id.dice).setOnClickListener {
             /*  if(Courutine_Dice.isActive)*/
             /* Courutine_Dice =*/ MainScope().launch {
 
-
+            var SoundBuffer = Random.nextInt(1..6)
             var rotateDirection = arrayListOf(-1, 1).random()//
             var random_Buffer_XY = Random.nextBoolean()
             var rotation_Time: Long = 1
@@ -55,10 +67,8 @@ class MainActivity : AppCompatActivity() {
                 }
 
 
-
-
                 findViewById<ImageView>(R.id.dice).setImageResource(
-                    when ((1..6).random()) {
+                    when (SoundBuffer) {
                         1 -> R.drawable.one
                         2 -> R.drawable.two
                         3 -> R.drawable.three
@@ -87,8 +97,41 @@ class MainActivity : AppCompatActivity() {
 
                 }
             }
+            play_sound_buffer = SoundBuffer
+            playSound(SoundBuffer)
+
 
         }
         }
+
+
+        soundPool = SoundPool(1, AudioManager.STREAM_MUSIC, 0)
+        soundId1 = soundPool!!.load(baseContext, R.raw.one, 1)
+        soundId2 = soundPool!!.load(baseContext, R.raw.two, 1)
+        soundId3 = soundPool!!.load(baseContext, R.raw.three, 1)
+        soundId4 = soundPool!!.load(baseContext, R.raw.four, 1)
+        soundId5 = soundPool!!.load(baseContext, R.raw.five, 1)
+        soundId6 = soundPool!!.load(baseContext, R.raw.six, 1)
+
+
+
     }
+
+    fun playSound(sound: Int) {
+        var soundId = 0
+        when (play_sound_buffer) {
+            1 -> soundId = soundId1
+            2 -> soundId = soundId2
+            3 -> soundId = soundId3
+            4 -> soundId = soundId4
+            5 -> soundId = soundId5
+            6 -> soundId = soundId6
+            else -> print("Error!")
+        }
+        soundPool?.play(soundId, 1F, 1F, 0, 0, 1F)
+
+
+    }
+
+
 }
